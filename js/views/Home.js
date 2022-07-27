124c6a388fe479ce70480f26ae5bfa453f7396b9
Zachary Johnson <zachary.a.johnson42@gmail.com>
2022-07-27 10:08:00 -0500

    working on displaying movies. fetch to get all appears to be working correctly. need to error catch for when movie objects are missing props.

--- js/views/Home.js
+++ js/views/Home.js
@@ -2,7 +2,12 @@
 // Lets just make some html that's nice, and links to our other stuff.
 
 export default function Home(props) {
-
+    return `
+        <h1> Welcome! </h1>
+        <div>
+        <p> WHat to do</p>
+</div>
+    `
 }
 
 // Will need to set buttons or a tags up to redirect from page, unless we just want to stick to the nav bar.
--- js/views/Movies.js
+++ js/views/Movies.js
@@ -12,7 +12,26 @@
 // To get all movies made, we'll for loop through and each movie object into a card, then print them all to the DOM
 // Don't forget, each card needs a delete button attached to it!
 export default function MoviesView(props) {
+    // For loop through the response array, grab all the stuff I need with a fetch
+   console.log(props.allMovies)
 
+   let html = ''
+   for (let i = 0; i < props.allMovies.length; i++) {
+      html += `<div class="movie-card">`
+      html += props.allMovies[i].title
+      html +=`<div> ${props.allMovies[i].genre}`
+      html += `<div> ${props.allMovies[i].rating}`
+      html += `<div> ${props.allMovies[i].director} </div>`
+      html += `</div> </div> </div>`
+   }
+
+let allMovies = document.getElementById('allMovies')
+allMovies.innerHTML = html;
+
+    // Need to fill this
+   return `<div id="allMovies">
+    </div>
+`
 }
 
 
